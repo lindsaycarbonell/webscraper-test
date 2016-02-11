0604b3b62f89e411b259051d0a0f3b6ec10d2140
Lindsay Carbonell <lindsaycarbonell@gmail.com>
2016-02-11 16:57:01 -0500

    can pull titles, price range, word count for first page

--- index.js
+++ index.js
@@ -8,14 +8,41 @@ request(url, function (error, response, body) {
       var $ = cheerio.load(body);
       var titles = $("[rel='bookmark']");
       var all_titles = [];
+      //get all the titles of news orgs
       titles.each(function (i, title) {
-      //  console.log(title);
-        //title.html();
         all_titles.push($(title).html());
       });
 
+      var entry_contents = $(".entry-content");
+      var all_entries = [];
+      var all_prices = [];
+      var all_ranges = [];
+
+      entry_contents.each(function (i, entry) {
+        all_entries.push($(entry).html());
+      });
+
+      all_entries.forEach(function (entry, i){
+        var this_entry = $(entry).html();
+        //console.log(this_entry);
+        var thisPrice = this_entry.match(/\$\d,?\d*( per word)?/);
+        //
+        var thisRange = this_entry.match(/\d,?\d*- to \d,?\d*/);
+
+        all_prices.push(thisPrice[0]);
+        all_ranges.push(thisRange[0]);
+        //console.log(thisRange[0]);
+        //console.log(thisPrice[0]);
+
+
+
+
+      });
+
+      console.log(all_ranges);
+      console.log(all_prices);
       console.log(all_titles);
-        //titles = $("[rel='bookmark']").html();
+
 
 
 
