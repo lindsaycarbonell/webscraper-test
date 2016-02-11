c4553ed1f498236ca3be5ca8a90c3bac5500b5b7
Lindsay Carbonell <lindsaycarbonell@gmail.com>
2016-02-11 16:06:02 -0500

    can now pull all titles from a WP page

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 var request = require("request"),
   cheerio = require("cheerio"),
-  url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 02888;
+  url = "http://whopays.scratchmag.net/page/" + 1;
 
 request(url, function (error, response, body) {
   if (!error) {
-    var $ = cheerio.load(body),
-      temperature = $("[data-variable='temperature'] .wx-value").html();
+      // load the body of the page into Cheerio so we can traverse the DOM
+      var $ = cheerio.load(body);
+      var titles = $("[rel='bookmark']");
+      var all_titles = [];
+      titles.each(function (i, title) {
+      //  console.log(title);
+        //title.html();
+        all_titles.push($(title).html());
+      });
 
-    console.log("It’s " + temperature + " degrees Fahrenheit.");
+      console.log(all_titles);
+        //titles = $("[rel='bookmark']").html();
+
+
+
+
+
+
   } else {
     console.log("We’ve encountered an error: " + error);
   }
